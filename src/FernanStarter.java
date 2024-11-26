008b28bf3ce7ca4eb16945716ec2787411b6c188
Agarpinho <agajesus12@gmail.com>
2024-11-26 19:47:51 +0100

    panel de control

--- src/FernanStarter.java
+++ src/FernanStarter.java
@@ -6,15 +6,18 @@ public class FernanStarter {
     String usuarioAdministrador="Administrador";
     String contraAdministrador="1234";
         int opcionAdmi = 0;
+        int menuadmi = 0;
+        String nombreUsuario = "";
+    //UsuarioGestor
     String usuarioGestor="Gestor";
     String contraGestor="4321";
-    boolean bloqueadoGestor=false;
+        boolean bloqueadoGestor=false;
     String inversor1="Inversor1";
     String inversorContra="Inversor1";
-    boolean bloqueadoInversor1=false;
+        boolean bloqueadoInversor1=false;
     String inversor2="Inversor2";
     String inversor2Contra="Inversor2";
-    boolean bloqueadoInversor2=false;
+        boolean bloqueadoInversor2=false;
     int intentos=0;
     boolean acceso=false;
 
@@ -28,14 +31,59 @@ public class FernanStarter {
                 if(contraPedido.equals(contraAdministrador)){
                     System.out.println("Login realizado. Bienvenido "+usuarioAdministrador);
                     acceso=true;
-                     opcionAdmi = sc.nextInt();
-                    System.out.println("1. Panel de control");
-                    System.out.println("2. Proyectos");
-                    System.out.println("3. Configuración");
-                    System.out.println("4. Cerrar sesión");
-                    switch (opcionAdmi){
-                            
-                    }
+                    do {
+                        System.out.println("1. Panel de control");
+                        System.out.println("2. Proyectos");
+                        System.out.println("3. Configuración");
+                        System.out.println("4. Cerrar sesión");
+                        opcionAdmi = sc.nextInt();
+
+                        do {
+                            switch (opcionAdmi) {
+                                case 1:
+                                    System.out.println("Bienvenido al Panel de control que deseas");
+                                    System.out.println("1.Bloquear");
+                                    System.out.println("2.Desbloquear");
+                                    System.out.println("3.Salir");
+                                    menuadmi = sc.nextInt();
+                                    switch (menuadmi) {
+                                        case 1:
+                                            System.out.println("A quien deseas bloquear " + usuarioGestor + ", " + inversor1 + ", " + inversor2);
+                                            nombreUsuario = sc.next();
+                                            if (nombreUsuario.equals(usuarioGestor)) {
+                                                bloqueadoGestor = true;
+                                                System.out.println("Se ha bloqueado exitosamente al " + usuarioGestor);
+                                            } else if (nombreUsuario.equals(inversor1)) {
+                                                bloqueadoInversor1 = true;
+                                                System.out.println("Se ha bloqueado exitosamente al " + inversor1);
+                                            } else if (nombreUsuario.equals(inversor2)) {
+                                                bloqueadoInversor2 = true;
+                                                System.out.println("Se ha bloqueado exitosamente al " + inversor2);
+                                            }
+                                            break;
+                                        case 2:
+                                            System.out.println("A quien deseas desbloquear " + usuarioGestor + ", " + inversor1 + ", " + inversor2);
+                                            nombreUsuario = sc.next();
+                                            if (nombreUsuario.equals(usuarioGestor)) {
+                                                bloqueadoGestor = false;
+                                                System.out.println("Se ha desbloqueado exitosamente al " + usuarioGestor);
+                                            } else if (nombreUsuario.equals(inversor1)) {
+                                                bloqueadoInversor1 = false;
+                                                System.out.println("Se ha desbloqueado exitosamente al " + inversor1);
+                                            } else if (nombreUsuario.equals(inversor2)) {
+                                                bloqueadoInversor2 = false;
+                                                System.out.println("Se ha desbloqueado exitosamente al " + inversor2);
+                                            }
+                                            break;
+                                        case 3:
+                                            System.out.println("Adios");
+                                            break;
+                                    }
+                                    break;
+
+                            }
+                        } while (menuadmi != 3);
+                    }while (opcionAdmi != 4);
                 }else{
                     System.out.println("Login fallido");
                 }
